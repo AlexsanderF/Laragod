e6f6716dd5e6309f819cae2072e5485cd38e4a3d
Alexsander Fontes <sander-369@hotmail.com>
2024-04-22 21:31:08 -0300

    :sparkles: feat: conditional empty and isset.

--- resources/views/examples/conditionals_switch.blade.php
+++ resources/views/examples/conditionals_switch.blade.php
@@ -1,40 +1,47 @@
-@switch($month)
-    @case(1)
-        <p>Janeiro</p>
-        @break
-    @case(2)
-        <p>Fevereiro</p>
-        @break
-    @case(3)
-        <p>Março</p>
-        @break
-    @case(4)
-        <p>Abril</p>
-        @break
-    @case(5)
-        <p>Maio</p>
-        @break
-    @case(6)
-        <p>Junho</p>
-        @break
-    @case(7)
-        <p>Julho</p>
-        @break
-    @case(8)
-        <p>Agosto</p>
-        @break
-    @case(9)
-        <p>Setembro</p>
-        @break
-    @case(10)
-        <p>Outubro</p>
-        @break
-    @case(11)
-        <p>Novembro</p>
-        @break
-    @case(12)
-        <p>Dezembro</p>
-        @break
-    @default
-    <h1>Você digitou {{$month}}, não existe um mês associado com essa numeração >:(</h1>
-@endswitch
+@isset($month)
+
+    @switch($month)
+        @case(1)
+            <p>Janeiro</p>
+            @break
+        @case(2)
+            <p>Fevereiro</p>
+            @break
+        @case(3)
+            <p>Março</p>
+            @break
+        @case(4)
+            <p>Abril</p>
+            @break
+        @case(5)
+            <p>Maio</p>
+            @break
+        @case(6)
+            <p>Junho</p>
+            @break
+        @case(7)
+            <p>Julho</p>
+            @break
+        @case(8)
+            <p>Agosto</p>
+            @break
+        @case(9)
+            <p>Setembro</p>
+            @break
+        @case(10)
+            <p>Outubro</p>
+            @break
+        @case(11)
+            <p>Novembro</p>
+            @break
+        @case(12)
+            <p>Dezembro</p>
+            @break
+        @default
+            <h1>Você digitou {{$month}}, não existe um mês associado com essa numeração >:(</h1>
+    @endswitch
+@endisset
+
+@empty($month)
+    <h1>Parece que você não digitou nada, é isso mesmo que queria me passar?</h1>
+@endempty
